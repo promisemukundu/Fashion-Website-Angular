946c38e1b33e4b9abd9813c38d836b370d4cbb08
Innocent Mazando <innocent.mazando@falcorp.co.za>
2023-06-15 13:12:56 +0200

    Some staff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { ShopComponent } from './shop/shop.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogDetailsComponent } from './blog/blog-details/blog-details.component';
+import { ShopDetailsComponent } from './shop/shop-details/shop-details.component';
+import { ShoppingCartComponent } from './shop/shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'blog-details/:id', component: BlogDetailsComponent },
+  { path: 'shop-details', component: ShopDetailsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,73 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CorouselComponent } from './corousel/corousel.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ShopComponent } from './shop/shop.component';
+import { ShopDetailsComponent } from './shop/shop-details/shop-details.component';
+import { ShoppingCartComponent } from './shop/shopping-cart/shopping-cart.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogDetailsComponent } from './blog/blog-details/blog-details.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { SummerCollectionsComponent } from './summer-collections/summer-collections.component';
+import { ProductComponent } from './product/product.component';
+import { InstagramComponent } from './instagram/instagram.component';
+import { NewsComponent } from './news/news.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SearchPipe } from './search.pipe';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    CorouselComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    ShopComponent,
+    ShopDetailsComponent,
+    ShoppingCartComponent,
+    BlogComponent,
+    BlogDetailsComponent,
+    CheckoutComponent,
+    AboutUsComponent,
+    ContactComponent,
+    CanvasComponent,
+    SummerCollectionsComponent,
+    ProductComponent,
+    InstagramComponent,
+    NewsComponent,
+    SearchPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CarouselModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
+  bootstrap: [AppComponent],
+  exports: [SearchPipe],
 })
 export class AppModule { }
